1c3212decb7bdf341a20422774876c5b5074601f
mahmoudRiad22 <mahmoudriad027@gmail.com>
2023-08-10 00:08:02 -0700

    adding task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * DEscription: A c-program responding to task 4 on alx
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
