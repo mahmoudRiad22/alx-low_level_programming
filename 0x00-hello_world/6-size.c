f8a2839a359e29c43c47d8e753c2d1fbfa31a2a8
mahmoudRiad22 <mahmoudriad027@gmail.com>
2023-08-10 00:58:41 -0700

    adding task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * decription: a c-program to answer the task 6 on alx
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+printf("Size of a char: %zu byte(s)\n", (size_t)sizeof(char));
+printf("Size of an int: %zu byte(s)\n", (size_t)sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", (size_t)sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", (size_t)sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", (size_t)sizeof(float));
+return (0);
+}
