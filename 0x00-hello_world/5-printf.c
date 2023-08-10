f1e25928df46f55acd36b627bdf4f1256f3d4601
mahmoudRiad22 <mahmoudriad027@gmail.com>
2023-08-10 00:18:09 -0700

    adding task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Decription: a c-program to answer the task 5 on alx
+ *
+ * Return: always 0 (sucess)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
