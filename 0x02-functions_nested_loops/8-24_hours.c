2c2cd91e67393bc39e2c1af095cd8a509b3119f8
mahmoudRiad22 <mahmoudriad027@gmail.com>
2023-08-15 09:52:35 -0700

    adding task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * jack_bauer - print every minute fot eh day for jack bauer
+ *
+ * Parameters: void
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int h1 = 0, h2 = 0, m1 = 0, m2 = 0;
+	int stop = 0;
+
+	while (stop == 0)
+	{
+		_putchar(h1 + '0');
+		_putchar(h2 + '0');
+		_putchar(':');
+		_putchar(m1 + '0');
+		_putchar(m2 + '0');
+		_putchar('\n');
+		m2++;
+
+		if (m2 > 9)
+		{
+			m1++;
+			m2 = 0;
+		}
+		if (m1 > 5)
+		{
+			h2++;
+			m1 = 0;
+		}
+		if (h2 > 3)
+		{
+			h1++;
+			h2 = 0;
+		}
+		if (h1 > 2)
+		{
+			h1 = 0;
+			stop = 1;
+		}
+	}
+}
