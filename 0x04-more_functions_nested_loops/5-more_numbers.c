28c27cfd178bc5bfe1544a0917026863b012807f
mahmoudRiad22 <mahmoudriad027@gmail.com>
2023-08-17 02:55:00 -0700

    fixing an error on task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,9 +15,15 @@ void more_numbers(void)
 	{
 		i = 0;
 
-		while (i < 10)
+		while (i <= 14)
 		{
-			_putchar(i + '0');
+			if (i < 10)
+				_putchar(i + '0');
+			else 
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+			}
 			i++;
 		}
 		_putchar('\n');
