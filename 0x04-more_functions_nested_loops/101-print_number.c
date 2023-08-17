77265823d5746c1d7c807df9934ec6ab0cd88c94
mahmoudRiad22 <mahmoudriad027@gmail.com>
2023-08-17 14:14:59 -0700

    fixing an arror on task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,69 +1,27 @@
 #include "main.h"
-/**
- * print_number - a function that print any integer
- *
- * @n:number to be printed
- *
- * Return: void
- */
 
+/**
+ * print_number - prints an integer
+ * @n: integer to be printed
+ */
 void print_number(int n)
 {
-	/*if (n <= 2147483647 || n >= -2147483647)*/
+	unsigned int n1;
+
+	if (n < 0)
 	{
-		unsigned int filter = 1, out, Len;
-
-		if (n == 0)
-		{
-			_putchar('0');
-			/*_putchar('\n');*/
-		}
-		else
-		{
-		if (n < 0)
-		{ /*remove negative sign after printing it*/
-			_putchar('-');
-			n = n / -1;
-		}
-		Len = len(n);
-
-		while (Len - 1 > 0)
-		{
-			Len--;
-			filter = filter * 10;
-		}
-
-		/*filter = filter / 10;reduce filter digits by 1*/
-
-		while (filter > 1)
-		{
-			out = n / filter;
-			n = n % filter;
-			filter = filter / 10;/*adjust filter size */
-			_putchar(out + '0');
-		}
-		_putchar(n + '0');
-		}
-	}
-}
-
-/**
- * len - return the number of digits in a given number
- *
- * @n: given number
- *
- * Return: number of digits
- */
-
-int len(int n)
-{
-	int Len = 0;
-
-	while (n > 0)
+		n1 = -n;
+		_putchar('-');
+	} else
 	{
-		Len++;
-		n = n / 10;
+		n1 = n;
 	}
 
-	return (Len);
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+
+	_putchar((n1 % 10) + '0');
 }
+
