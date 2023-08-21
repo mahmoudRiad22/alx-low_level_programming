4664bce676c4c3da4f5b99103a37b0e921cb6872
mahmoudRiad22 <mahmoudriad027@gmail.com>
2023-08-21 12:32:37 -0700

    trying for task 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - generates a random password  for the program 101-crackme
+ *
+ * @: void
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int digits = 0;
+	char Random;
+	unsigned int size = 0;
+
+	srand(time(NULL));
+
+	while (size < 32767)/*cuz its the lowest value of RAND_MAX*/
+	{
+	Random = rand();
+	printf("%c", Random);
+	size += Random;
+	digits++;
+	}
+
+return (0);
+}
