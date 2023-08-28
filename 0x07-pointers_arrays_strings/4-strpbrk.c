5818ebddfec92f90e58e894e1b352910e2d01cf6
mahmoudRiad22 <mahmoudriad027@gmail.com>
2023-08-28 23:27:06 +0300

    fixing betty error

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * function that searches a string for any of a set of bytes
+ * _strpbrk - function that searches a string for any of a set of bytes
  *
  * @s: given string
  * @accept: string of bytes to search for
