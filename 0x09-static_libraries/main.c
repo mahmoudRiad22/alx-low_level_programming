f34215e874b570f6c86c5d6a4d68091d1604954b
mahmoudRiad22 <mahmoudriad027@gmail.com>
2023-09-01 07:37:08 -0700

    Fixing error on task 0

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -9,6 +9,6 @@
 int main(void)
 {
 	_puts("\"At the end of the day, my go\
-			al was to be the best hacker\"\n\t- Kevin Mitnick");
+al was to be the best hacker\"\n\t- Kevin Mitnick");
 	return (0);
 }
