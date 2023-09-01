f663069d8e8874f8c9712132a19d6e5fcb853b47
mahmoudRiad22 <mahmoudriad027@gmail.com>
2023-09-01 12:42:57 -0700

    adding task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+int _atoi(char *s);
+
+/**
+ * main - entry point
+ *
+ * @argc: number of arguments in the command line
+ * @argv: array of pinters to each argument
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, number = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		number += _atoi(argv[i]);
+		/*printf("%d\n", number);*/
+	}
+	printf("%d\n", number);
+	return (0);
+}
+
+/**
+ * _atoi - a function that convert a string to an integer
+ *
+ * @s: given string
+ *
+ * Return: the integer value
+ */
+
+int _atoi(char *s)
+{
+	int negatives = 0, flag = 0;
+	unsigned int x = 0;
+
+	if (*s == '\0')
+		return (0);
+	{
+		while (*s != '\0')
+		{
+			if (*s == '-')
+			{
+				negatives++;
+				s++;
+				continue;
+			}
+
+			if (*s >= '0' && *s <= '9')
+			{
+				flag++;
+
+				while (*s >= '0' && *s <= '9')
+				{
+					x = (x * 10) + ((*s) - '0');
+					s++;
+				}
+				break;
+			}
+			s++;
+		}
+	}
+if (flag == 0)
+	return (0);
+if (negatives % 2 != 0)
+	return (-x);
+else
+	return (x);
+
+}
