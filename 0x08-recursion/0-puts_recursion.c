b3dd0fffec32649f73c4846f9eaadc282ce1e9da
mahmoudRiad22 <mahmoudriad027@gmail.com>
2023-10-26 04:30:17 +0300

    adding the project to the repo

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _puts_recursion - a funtion that print a string + newline
+ *
+ * @s: string adress to be printed
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	char *next = s;
+
+	if (*next != '\0')
+		_putchar(*next);
+	else if (*next == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	next = s + 1;
+	_puts_recursion(next);
+}
