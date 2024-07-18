d3b3632b70381b62089893f3174a99c65fb575a3
mahmoudRiad22 <mahmoudriad027@gmail.com>
2024-07-18 16:40:55 +0300

    fixing output errors

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,12 +9,14 @@
 */
 int linear_search(int *array, size_t size, int value)
 {
+	size_t i;
+
 	if (array == NULL)
 	{ return (-1); }
 
-	for (size_t i = 0; i < size; i++)
+	for(i = 0; i < size; i++)
 	{
-	printf("Value checked array[%zu] = [%d]", i, array[i]);
+	printf("Value checked array[%lu] = [%d]\n", (unsigned long)i, array[i]);
 	if (array[i] == value)
 	{ return (i); }
 	}
