29922f621d68bf2d7cbc0250260cf9dc60e6086b
mahmoudRiad22 <mahmoudriad027@gmail.com>
2024-07-18 17:24:29 +0300

    still checker 8 is an error

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,13 +12,17 @@ int linear_search(int *array, size_t size, int value)
 	int i;
 
 	if (array == NULL)
-	{ return (-1); }
-
-	for(i = 0; i < (int)size; i++)
 	{
-	printf("Value checked array[%u] = [%d]\n", i, array[i]);
-	if (array[i] == value)
-	{ return (i); }
+		return (-1);
+	}
+
+	for (i = 0; i < (int)size; i++)
+	{
+		printf("Value checked array[%u] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
 	}
 
 	return (-1);
